724814cc0221a983968c9067d8f292aaf54a6655
bodo2020 <elmadawymarket@gmail.com>
2026-09-14 11:18:35 +0300

    feat(staff): initialize native safe area and back behavior

--- apps/staff/src/main.tsx
+++ apps/staff/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import NativeBarcodeFab from "./components/NativeBarcodeFab";
 import { installStaffAuthEnhancements } from "./authEnhancer";
+import { installNativeAppBridge } from "./nativeAppBridge";
 import "./styles.css";
 import "./picking.css";
+import "./native-safe-area.css";
 
 installStaffAuthEnhancements();
+void installNativeAppBridge();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
