5775b00e940058156585684ceb65167a23f858ad
bodo2020 <elmadawymarket@gmail.com>
2026-09-14 10:53:03 +0300

    fix(staff): enable username auth mapping and password eye

--- apps/staff/src/main.tsx
+++ apps/staff/src/main.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import NativeBarcodeFab from "./components/NativeBarcodeFab";
+import { installStaffAuthEnhancements } from "./authEnhancer";
 import "./styles.css";
 import "./picking.css";
+
+installStaffAuthEnhancements();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
