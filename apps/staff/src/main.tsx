8cafdea01034639e67819a9579408a885d5b7476
bodo2020 <elmadawymarket@gmail.com>
2026-09-14 14:36:36 +0300

    feat(staff): load staging workspace styles

--- apps/staff/src/main.tsx
+++ apps/staff/src/main.tsx
@@ -12,6 +12,7 @@ import "./picking.css";
 import "./native-safe-area.css";
 import "./staff-v05.css";
 import "./substitutions.css";
+import "./staging.css";
 
 installStaffAuthEnhancements();
 void installNativeAppBridge();
