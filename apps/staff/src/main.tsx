f9c866bbe0a75f65a3ff3f78a8b103e0174c1134
bodo2020 <elmadawymarket@gmail.com>
2026-09-14 10:27:36 +0300

    feat(staff): mount native barcode camera button

--- apps/staff/src/main.tsx
+++ apps/staff/src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import NativeBarcodeFab from "./components/NativeBarcodeFab";
 import "./styles.css";
 import "./picking.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode>
+    <App />
+    <NativeBarcodeFab />
+  </React.StrictMode>,
 );
